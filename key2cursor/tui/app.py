cfa603557179273a4e13d8483a74218f9654c517
Cell1729 <sabax256256@gmail.com>
2025-05-20 16:17:01 +0900

    update : TUIのレイアウト修正
    マウス画面のTUIを調整

--- key2cursor/tui/app.py
+++ key2cursor/tui/app.py
@@ -1,8 +1,12 @@
 from textual.app import App
 from textual.binding import Binding
 from textual.widgets import Footer, Header, Button, Label, ListItem, ListView
-from textual.containers import Horizontal, Vertical
+from textual.containers import Horizontal, Vertical, Container
 
+# memo
+# ListViewで現在のキーボードを表示する
+# Buttonでマウスのボタンごとにキーバインドを表示する。
+# 具体的にはマウスボタンを押下、したときにキーバインドを入力する画面を作成し入力できるようにする。
 
 class AppTest(App):
     TITLE = "Key2Cursor"
@@ -17,29 +21,46 @@ class AppTest(App):
     def compose(self):
         yield Header()
         yield Footer()
-        yield ListView(
-            ListItem(Label("Left Click")),
-            ListItem(Label("Right Click")),
-            ListItem(Label("Scroll Up")),
-            ListItem(Label("Scroll Down")),
-            ListItem(Label("Side Button 1")),
-            ListItem(Label("Side Button 2")),
+        yield Container(
+                ListView(
+                ListItem(Label("Left Click - <mouse mode + left>")),
+                ListItem(Label("Right Click - <mouse mode + right>")),
+                ListItem(Label("Scroll Up - <mouse mode + up>")),
+                ListItem(Label("Scroll Down - <mouse mode + down>")),
+                ListItem(Label("Side Button 1 - <mouse mode + side1>")),
+                ListItem(Label("Side Button 2 - <mouse mode + side2>")),
+            ),
+            classes="bordered-list",
         )
-        yield Horizontal(
-            Vertical(
-                Button("Side1", id="side_button1"),
-                Button("Side2", id="side_button2"),
+        yield Container(
+            Horizontal(
+                Vertical(
+                    Button("Side1", id="side_button1"),
+                    Button("Side2", id="side_button2"),
+                    classes="side-buttons",
+                ),
+                Container(
+                    Horizontal(
+                        Vertical(
+                            Button("Left Click", id="left_click"),
+                        ),
+                        Vertical(
+                            Button("Scroll Up", id="scroll_up"),
+                            Button("Scroll Down", id="scroll_down"),
+                        ),
+                        Vertical(
+                            Button("Right Click", id="right_click"),
+                        ),
+                        classes="main-mouse-buttons",
+                    ),
+                    classes="main-mouse-frame",
+                ),
+                Vertical(
+                    Button("Mouse Mode", id="mouse_mode"),
+                    classes="mouse-mode",
+                ),
             ),
-            Vertical(
-                Button("Left Click", id="left_click"),
-            ),
-            Vertical(
-                Button("Scroll Down", id="scroll_down"),
-                Button("Scroll Up", id="scroll_up"),
-            ),
-            Vertical(
-                Button("Right Click", id="right_click"),
-            )
+            classes="bordered-buttons",
         )
 
 if __name__ == "__main__":
